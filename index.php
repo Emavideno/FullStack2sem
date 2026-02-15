8c66fefb40a30badbb84e44e2f2d62641c69db86
Ainur <ainur9173944784@gmail.com>
2026-02-15 21:49:35 +0300

    Profile Page
    1 homework

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+echo "<h1>Добрый день, ";
+
+$role = $_GET['role'];
+if ($role == 'admin') {
+    $safeRole = htmlspecialchars($role, ENT_QUOTES | ENT_SUBSTITUTE, 'UTF-8');
+    echo "$safeRole ";
+}
+
+$name = $_GET['name'] ?? 'Гость';
+$safeName = htmlspecialchars($name, ENT_QUOTES | ENT_SUBSTITUTE, 'UTF-8');
+echo "$safeName</h1>";
+
+echo "<h2> Был использован метод: " . $_SERVER["REQUEST_METHOD"] . " </h2>";
